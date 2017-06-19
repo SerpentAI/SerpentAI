3c17b7ad6f036c230a9019506ca6f703d2d7c923
Dgc2002 <dgc2002@gmail.com>
2017-06-18 20:41:54 -0500

    Added support for passing arguments when starting a game through the Steam Protocol

--- lib/game_launchers/steam_game_launcher.py
+++ lib/game_launchers/steam_game_launcher.py
@@ -13,13 +13,20 @@ class SteamGameLauncher(GameLauncher):
 
     def launch(self, **kwargs):
         app_id = kwargs.get("app_id")
+        app_args = kwargs.get("app_args")
 
         if app_id is None:
             raise GameLauncherException("An 'app_id' kwarg is required...")
 
+        proto_string = f"steam://run/{app_id}"
+
+        if app_args is not None:
+            args_list = ["--{}={}".format(k, v) for k, v in app_args.items()]
+            proto_string += "/en/" + " ".join(args_list)
+
         if sys.platform in ["linux", "linux2"]:
-            subprocess.call(shlex.split(f"xdg-open steam://run/{app_id}"))
+            subprocess.call(shlex.split(f"xdg-open {proto_string}"))
         elif sys.platform == "darwin":
-            subprocess.call(shlex.split(f"open steam://run/{app_id}"))
+            subprocess.call(shlex.split(f"open {proto_string}"))
         elif sys.platform == "windows":
-            webbrowser.open(f"steam://run/{app_id}")
+            webbrowser.open(f"{proto_string}")
