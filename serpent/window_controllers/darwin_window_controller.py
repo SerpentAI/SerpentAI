ef24fd0ca75a4d51594adc6d665c3ac587ec6cee
Nicholas Brochu <info@nicholasbrochu.com>
2017-09-26 07:26:42 -0400

    revert the applescript for is_window_focused to original; code is fine, users just need to use the process name on macOS instead of the window title

--- serpent/window_controllers/darwin_window_controller.py
+++ serpent/window_controllers/darwin_window_controller.py
@@ -34,23 +34,9 @@ class DarwinWindowController(WindowController):
 
     def is_window_focused(self, window_id):
         focused_window_id = applescript.AppleScript('''
-            global frontApp, frontAppName, windowTitle
-
-            set windowTitle to ""
             tell application "System Events"
-                set frontApp to first application process whose frontmost is true
-                set frontAppName to name of frontApp
-                set windowTitle to "no window"
-                tell process frontAppName
-                    if exists (1st window whose value of attribute "AXMain" is true) then
-                        tell (1st window whose value of attribute "AXMain" is true)
-                            set windowTitle to value of attribute "AXTitle"
-                        end tell
-                    end if
-                end tell
+                return name of first application process whose frontmost is true
             end tell
-            
-            return windowTitle
         ''').run()
 
         return focused_window_id == window_id
