b67b22d1d4edaf61c9a7db4acba012c7721d633c
Morgan Giraud <morgan@explee.com>
2017-11-22 10:10:58 +0100

    Make darwin_window_controller more robust

--- serpent/window_controllers/darwin_window_controller.py
+++ serpent/window_controllers/darwin_window_controller.py
@@ -1,5 +1,6 @@
 from serpent.window_controller import WindowController
 
+import re
 import applescript
 
 
@@ -13,34 +14,28 @@ class DarwinWindowController(WindowController):
             set visibleWindows to {}
             tell application "System Events"
                 repeat with this_app in (get processes whose background only is false) --get applications with 1+ window
-                    tell this_app
-                        set p_name to name
-                    end tell
-                    repeat with this_window in (get windows of this_app)
-                        tell this_window
-                            set w_name to name
-                        end tell
-                        set end of visibleWindows to {p_name, w_name}
-                    end repeat
+                    set end of visibleWindows to { (name of this_app), (get name of every window of this_app) }
                 end repeat
             end tell
             return visibleWindows
         ''').run()
-        for (p_name, w_name) in all_windows:
-            if w_name == name:
-                return (p_name, w_name)
-        return ["", ""]
+        for p_name, windows in all_windows:
+            for i, w_name in enumerate(windows):
+                if re.search(".*" + name + ".*", w_name):
+                    return (p_name, i + 1) # Windows are ordered by order of creations, it's a value that is fixed for the duration of the window's life
+        # If we didn't found the window, we fallback by testing if name is a process name
+        return [name, 1]
 
     def move_window(self, window_id, x, y):
         applescript.AppleScript(f'''
-            tell application "System Events" to tell window "{window_id[1]}" of process "{window_id[0]}"
+            tell application "System Events" to tell window {window_id[1]} of process "{window_id[0]}"
                 set position to { {x}, {y} }
             end tell
         ''').run()
 
     def resize_window(self, window_id, width, height):
         applescript.AppleScript(f'''
-            tell application "System Events" to tell window "{window_id[1]}" of process "{window_id[0]}"
+            tell application "System Events" to tell window {window_id[1]} of process "{window_id[0]}"
                 set size to { {width}, {height} }
             end tell
         ''').run()
@@ -53,7 +48,7 @@ class DarwinWindowController(WindowController):
         ''').run()
 
     def is_window_focused(self, window_id):
-        return self.get_focused_window_name() == window_id[1]
+        return window_id[0] == self.get_focused_window_name()
 
     def get_focused_window_name(self):
         focused_window_name = applescript.AppleScript('''
@@ -71,14 +66,14 @@ class DarwinWindowController(WindowController):
             return {frontAppName, windowTitle}
         ''').run()
 
-        return focused_window_name[1]
+        return focused_window_name[0]
 
     def get_window_geometry(self, window_id):
         geometry = dict()
 
         window_geometry = applescript.AppleScript(f'''
             tell application "System Events" to tell process "{window_id[0]}"
-                return get size of window "{window_id[1]}"
+                return get size of window {window_id[1]}
             end tell
         ''').run()
 
@@ -86,7 +81,7 @@ class DarwinWindowController(WindowController):
         geometry["height"] = int(window_geometry[1])
 
         window_information = applescript.AppleScript(f'''
-            tell application "System Events" to tell window "{window_id[1]}" of process "{window_id[0]}"
+            tell application "System Events" to tell window {window_id[1]} of process "{window_id[0]}"
                 return get position
             end tell
         ''').run()
