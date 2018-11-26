5e74d693cc84383b288bba37160307de817511cc
Grey Christoforo <grey@christoforo.net>
2018-11-26 00:48:11 +0000

    expose threshold setting for click_sprite

--- serpent/input_controllers/pyautogui_input_controller.py
+++ serpent/input_controllers/pyautogui_input_controller.py
@@ -230,9 +230,9 @@ class PyAutoGUIInputController(InputController):
             self.move(x=x, y=y)
             self.click(button=button, **kwargs)
 
-    def click_sprite(self, button=MouseButton.LEFT, sprite=None, game_frame=None, **kwargs):
+    def click_sprite(self, button=MouseButton.LEFT, sprite=None, game_frame=None, threshold = 0.85, **kwargs):
         if ("force" in kwargs and kwargs["force"] is True) or self.game_is_focused:
-            sprite_location = self.sprite_locator.better_locate(sprite=sprite, game_frame=game_frame)
+            sprite_location = self.sprite_locator.better_locate(sprite=sprite, game_frame=game_frame, threshold = threshold)
 
             if sprite_location is None:
                 return False
