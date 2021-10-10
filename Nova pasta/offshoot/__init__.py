9666cafe93cd6c8e92593bb074cf6a91de00a9d2
Martyn0324 <giovanni.ab@outlook.com>
2021-10-10 16:31:02 -0300

    Delete Nova pasta directory

--- Nova pasta/offshoot/__init__.py
+++
@@ -1,9 +0,0 @@
-from offshoot.base import *
-
-from offshoot.plugin import Plugin, PluginError
-from offshoot.pluggable import Pluggable
-from offshoot.manifest import Manifest
-
-
-config = load_configuration("offshoot.yml")
-pluggable_classes = lambda: map_pluggable_classes(config)
--- Nova pasta/scikit_image-0.14.2.dist-info/DESCRIPTION.rst
+++
@@ -1,9 +0,0 @@
-Image Processing SciKit
-
-Image processing algorithms for SciPy, including IO, morphology, filtering,
-warping, color manipulation, object detection, etc.
-
-Please refer to the online documentation at
-http://scikit-image.org/
-
-
