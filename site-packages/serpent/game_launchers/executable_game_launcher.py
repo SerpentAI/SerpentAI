75a7616a3f27b812e709e7b4e0eacf68d94ca25d
Martyn0324 <giovanni.ab@outlook.com>
2021-10-17 19:56:38 -0300

    Add files via upload

---
+++ site-packages/serpent/game_launchers/executable_game_launcher.py
@@ -0,0 +1,24 @@
+from serpent.game_launcher import GameLauncher, GameLauncherException
+from serpent.utilities import is_linux, is_macos, is_windows
+
+import shlex
+import subprocess
+
+
+class ExecutableGameLauncher(GameLauncher):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def launch(self, **kwargs):
+        executable_path = kwargs.get("executable_path")
+
+        if executable_path is None:
+            raise GameLauncherException("An 'executable_path' kwarg is required...")
+
+        if is_linux():
+            subprocess.Popen(shlex.split(executable_path))
+        elif is_macos():
+            subprocess.Popen(shlex.split(executable_path))
+        elif is_windows():
+            subprocess.Popen(shlex.split(executable_path))
